36da77b2f06bb828977308c0ec00e2f8a2b18c6c
root <cs736@gmail.com>
2016-04-20 14:08:50 -0400

    mmapcall: 1) Added support for ftracing the mmap calls 2) Added the number of pages to mmap as a commandline argument 3) Printing all measurements for reference

--- Tests/simple_mmmap_call/mmapcall.c
+++ Tests/simple_mmmap_call/mmapcall.c
@@ -25,6 +25,10 @@
 #include "bench.h"
 #include "parameters.h"
 
+#if (TRACE_MODE == 1)
+#include "tracing.h"
+#endif
+
 //int nbufs = NUM_PAGES_TO_TOUCH;
 int nbufs = 255;
 char *shared_area = NULL;
@@ -34,12 +38,21 @@ uint64_t *measurements;
 void usage()
 {
   printf(
-  "Usage: ./mmapcall <filename_base> <numfiles> \n"
+  "Usage: ./mmapcall <num_pages_tomap> <filename_base> <numfiles> \n"
   ""
   " note that filename base specifies the prefix for mmaped files, for ex -\n"
-  "   ./mmapcall ./data/files 100 \n"
-  " will access ./data/files0 through ./data/files99\n");
+  "   ./mmapcall 1024 ./data/files 100 \n"
+  " will access ./data/files0 through ./data/files99 and map 1024 pages each\n");
   exit(-1);
+}
+
+void print_measurements(uint64_t* marray, int size)
+{
+  int i;
+  printf("Meaurements = \n");
+  for(i = 0; i < size; i++) {
+    printf("%ld\n", marray[i]);
+  }
 }
 
 uint64_t stat_average(uint64_t* marray, int size)
@@ -59,9 +72,10 @@ main(int argc, char **argv)
     uint64_t start, end, nsec;
     char filename[1024];
 
-    if (argc > 2) {
-        filebase = argv[1];
-        numfd = atoi(argv[2]);
+    if (argc > 3) {
+        nbufs = atoi(argv[1]);
+        filebase = argv[2];
+        numfd = atoi(argv[3]);
     }
     else
       usage();
@@ -70,19 +84,32 @@ main(int argc, char **argv)
     measurements = (uint64_t*) malloc(numfd * sizeof(uint64_t));
     affinity_set(1);   
 
+    #if (TRACE_MODE == 1)
+    setup_trace();
+    #endif
+
     for(i=0; i < numfd; i++) {
       snprintf(filename, 1024, "%s%d", filebase, i);
       fd = open(filename, O_RDONLY);
 
+      #if (TRACE_MODE == 1)
+      trace_on();
+      #endif
+
       start = read_tsc();
       shared_area = mmap(0, (1 + nbufs) * 4096, PROT_READ, MMAP_FLAG, fd, 0);
       end = read_tsc();
+
+      #if (TRACE_MODE == 1)
+      trace_off();
+      #endif
 
       measurements[i] = end - start;
       //printf("measured %ld\n", end -start);
       close(fd); // closing the file does not unmap the region
     }
 
+    print_measurements(measurements, numfd);
     nsec = stat_average(measurements, numfd) * 1000000000 / get_cpu_freq();
     printf("nsec: %ld\t\n", nsec);
 
